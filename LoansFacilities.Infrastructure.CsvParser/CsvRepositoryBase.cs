61e252877e9be816e9c544800f2adf2d8633b4d1
Almaroo <mfxbarszcz@gmail.com>
2022-01-03 23:35:02 +0100

    Add exception handling

--- LoansFacilities.Infrastructure.CsvParser/CsvRepositoryBase.cs
+++ LoansFacilities.Infrastructure.CsvParser/CsvRepositoryBase.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using LogParser.Core.Interfaces;
 
 namespace LoansFacilities.Infrastructure.CsvParser
@@ -20,11 +22,30 @@ namespace LoansFacilities.Infrastructure.CsvParser
             _lineParser = lineParser;
         }
 
-        private StreamReader GetStreamReader() => new(_filePath);
+        private StreamReader GetStreamReader()
+        {
+            StreamReader streamReader = null;
+            
+            try
+            {
+                streamReader = new(_filePath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Failed to open file: ${_filePath}. Make sure it exists and is not used by other process");
+                Console.WriteLine(e);
+            }
+
+            return streamReader;
+        }
 
         protected IEnumerable<T> ReadLinesAndParseToCollectionOf<T>()
         {
             using var loansCsvFileReader = GetStreamReader();
+            
+            // TODO I would rather go with FP approach here with Option<> monad
+            if (loansCsvFileReader == null) return Enumerable.Empty<T>();
+            
             var result = new List<T>();
             string currentLine;
             var lineCount = 0;
@@ -35,8 +56,10 @@ namespace LoansFacilities.Infrastructure.CsvParser
             
             while ((currentLine = loansCsvFileReader.ReadLine()) != null)
             {
+                // tests each column against set validation rules
                 if (_lineParser.ValidateLineAndLogResult(++lineCount, currentLine))
                 {
+                    // this method makes use of reflection, that is why order in files is crucial
                     result.Add(_lineParser.ParseLineTo<T>(currentLine));
                 }
             }
